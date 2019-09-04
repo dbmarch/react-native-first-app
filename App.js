d5b83e6f72bd62e9c2ee9ce1cefe634f6cf54567
Dave March <dbmarch@gmail.com>
2019-09-03 20:26:16 -0400

    add flex styling

--- App.js
+++ App.js
@@ -1,31 +1,51 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import {  Text, View } from 'react-native';
+
+// Expo loads App from App.js
 
 export default function App() {
-  const [outputText, setOutputText] =  useState('Welcome to React Native!');
+  
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{outputText}</Text>
-      <Button style = {styles.button} title = "Change Text"  onPress = {()=>setOutputText('The new text')} />
+    <View style={{padding: 50, 
+          flexDirection: 'row',  
+          width: '80%', 
+          height: 300, 
+          justifyContent: 'space-around', 
+          alignItems: 'stretch'
+          
+          }}>
+      <View style = {{
+          backgroundColor: 'red',
+          flex: 3,
+          //width: 100,
+          // height: 100,
+          justifyContent: 'center',
+          alignItems: 'center',
+          // borderRadius: 10
+           }}>
+        <Text>1</Text>
+      </View> 
+      <View style = {{
+          backgroundColor: 'green',
+          flex: 1,
+          // width: 100,
+          // height: 100,
+           justifyContent: 'center',
+          alignItems: 'center'}} >
+          <Text>2</Text>
+        </View> 
+      <View  style = {{
+          backgroundColor: 'blue',
+          flex: 1,
+          // width: 100,
+          // height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'}}
+          >
+        <Text>3</Text>
+        </View> 
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'red'
-  },
-  button: {
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    backgroundColor: 'red'
-  }
-});
